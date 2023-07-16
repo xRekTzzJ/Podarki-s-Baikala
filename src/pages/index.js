69bd7138ebd5b0af745390870b2d70ff6e73f672
Pavel Popov <Pavel pavel2003sbk@yandex.ru>
2023-07-17 02:10:52 +0800

    feat: добавить прокрутку до каталога

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,11 @@ import { Popup } from '../components/Popup.js';
 //попап просмотра изображения
 const popupWithImage = new Popup('.popup_main');
 const mainImage = document.querySelector('.about-us__image');
-mainImage.src = './src/images/prospekt.jpeg'
+const mainImageSrc = mainImage.src = './src/images/prospekt.jpeg'
 mainImage.addEventListener('click', () => {
-    popupWithImage.open(mainImage.src)
+    popupWithImage.open(mainImageSrc)
 })
 popupWithImage.setEventListeners();
+const headerButton = document.querySelector('.header__button');
+const catalog = document.querySelector('.catalog');
+headerButton.addEventListener('click', () => catalog.scrollIntoView(true));
