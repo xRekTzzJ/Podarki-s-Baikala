9d553106cd74c599182d9a996e9d989107c3f03b
Pavel Popov <Pavel pavel2003sbk@yandex.ru>
2023-08-15 03:27:20 +0800

    feat: about us

---
+++ src/pages/about-us.js
@@ -0,0 +1,25 @@
+import { Popup } from '../components/Popup.js';
+
+const popupWithImage = new Popup('.popup_main');
+const images =["../images/prospekt-2.jpeg", "../images/кружка.jpeg", "../images/блокнот.jpeg",];
+let mainImage = document.querySelector('.main__image');
+mainImage.src = images[0];
+mainImage.addEventListener('click', () => {
+    popupWithImage.open(mainImage.src)
+})
+let numberOfMainImage = 0;
+const rightExpand = document.querySelector('.main__image-expand_right');
+const leftExpand = document.querySelector('.main__image-expand_left');
+rightExpand.addEventListener('click', () => {
+    if(numberOfMainImage < images.length-1){
+        numberOfMainImage++
+    }
+    mainImage.src = images[numberOfMainImage]
+})
+leftExpand.addEventListener('click', () => {
+    if(numberOfMainImage > 0){
+        numberOfMainImage--
+    }
+    mainImage.src = images[numberOfMainImage]
+})
+popupWithImage.setEventListeners();
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Popup } from '../components/Popup.js';
 //попап просмотра изображения
 const popupWithImage = new Popup('.popup_main');
 const mainImage = document.querySelector('.about-us__image');
-const mainImageSrc = mainImage.src = './src/images/prospekt.jpeg'
+const mainImageSrc = mainImage.src = './src/images/prospekt-2.jpeg'
 mainImage.addEventListener('click', () => {
     popupWithImage.open(mainImageSrc)
 })
