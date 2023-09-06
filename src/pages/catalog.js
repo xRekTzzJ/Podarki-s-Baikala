94817c91c8c0f551442166a86b4cfe8830a6cc80
Pavel Popov <Pavel pavel2003sbk@yandex.ru>
2023-09-06 12:47:29 +0800

    feat: new branch

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -2,63 +2,63 @@ import { Product } from "./Product.js"
 import { Section } from "./Section.js";
 const products = [
     {link: './perpetual-calendar.html',
-    image: "../images/календарь 2.jpeg",
+    image: "./src/images/календарь 2.jpeg",
     name: 'Вечные календари'
     },
     {link: './phone-stand.html',
-    image: "../images/подставка 1.jpeg",
+    image: "./src/images/подставка 1.jpeg",
     name: 'Подставки под телефон'
     },
     {link: './notebooks-catalog.html',
-    image: "../images/блокнот.jpeg",
+    image: "./src/images/блокнот.jpeg",
     name: 'Блокноты'
     },
     {link: './keychains-catalog.html',
-    image: "../images/brelok.jpg",
+    image: "./src/images/brelok.jpg",
     name: 'Брелоки'
     },
     {link: './panels.html',
-    image: "../images/panno.jpeg",
+    image: "./src/images/panno.jpeg",
     name: 'Панно'
     },
     {link: './cups-catalog.html',
-    image: "../images/кружка.jpeg",
+    image: "./src/images/кружка.jpeg",
     name: 'Кружки'
     },
     {link: './pendant.html',
-    image: "../images/подвеска.jpeg",
+    image: "./src/images/подвеска.jpeg",
     name: 'Подвески в машину'
     },
     {link: './gastronom.html',
-    image: "../images/Варенье-из-брусники-с-кедровым-орехом-125-гр-100-мл.jpg",
+    image: "./src/images/Варенье-из-брусники-с-кедровым-орехом-125-гр-100-мл.jpg",
     name: 'Гастрономические подарки'
     },
     {link: './ethno-panno-catalog.html',
-    image: "../images/этно-панно.jpeg",
+    image: "./src/images/этно-панно.jpeg",
     name: 'Этно-панно'
     },
     {link: './pillow.html',
-    image: "../images/подушка.JPG",
+    image: "./src/images/подушка.JPG",
     name: 'Подушки'
     },
     {link: './board.html',
-    image: "../images/доска 1.jpeg",
+    image: "./src/images/доска 1.jpeg",
     name: 'Доски'
     },
     {link: './banks-catalog.html',
-    image: "../images/копилка.jpeg",
+    image: "./src/images/копилка.jpeg",
     name: 'Копилки'
     },
     {link: './flash-drive.html',
-    image: "../images/флешка.jpeg",
+    image: "./src/images/флешка.jpeg",
     name: 'Флешки'
     },
     {link: './wineglass.html',
-    image: "../images/бокал 1.jpeg",
+    image: "./src/images/бокал 1.jpeg",
     name: 'Бокалы'
     },
     {link: './gift-bags.html',
-    image: "../images/подарочные мешочки.jpeg",
+    image: "./src/images/подарочные мешочки.jpeg",
     name: 'Подарочные мешочки'
     },
 ]
