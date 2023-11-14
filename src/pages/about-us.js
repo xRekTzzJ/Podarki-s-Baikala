06df618ab993a6b3d924458df98474dea088ff5c
Pavel Popov <Pavel pavel2003sbk@yandex.ru>
2023-11-15 00:34:36 +0800

    feat: добавил новые изображения

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,70 +1,79 @@
-import { Popup } from '../components/Popup.js';
-import { Footer } from '../components/Footer.js';
+import { Popup } from "../components/Popup.js";
+import { Footer } from "../components/Footer.js";
 const footerInfo = new Footer();
 footerInfo.text();
-const popupWithImage = new Popup('.popup_main');
-const images =["./src/images/алена.jpeg", "./src/images/prospekt-from-door.jpeg", "./src/images/prospekt-shop.jpeg", './src/images/баннер.jpeg', './src/images/команда.jpeg'];
-let mainImage = document.querySelector('.main__image');
-const popupImage = document.querySelector('.popup__image');
+const popupWithImage = new Popup(".popup_main");
+const images = [
+  "./src/images/prospekt-9.jpg",
+  "./src/images/prospekt-3.jpg",
+  "./src/images/prospekt-4.jpg",
+  "./src/images/prospekt-5.jpg",
+  "./src/images/prospekt-6.jpg",
+  "./src/images/prospekt-7.jpg",
+  "./src/images/prospekt-8.jpg",
+  "./src/images/команда.jpeg",
+];
+let mainImage = document.querySelector(".main__image");
+const popupImage = document.querySelector(".popup__image");
 let numberOfMainImage = 0;
 mainImage.src = images[0];
-popupImage.src = images[0]
-mainImage.addEventListener('click', () => {
-    popupWithImage.open(mainImage.src)
-})
-function nextImage(){
-    if(numberOfMainImage < images.length-1){
-        numberOfMainImage++
-    }
-    if(numberOfMainImage !== 0){
-        leftExpand.classList.remove('hidden')
-        leftPopupExpand.classList.remove('hidden')
-    }
-    if(numberOfMainImage === images.length-1){
-        rightExpand.classList.add('hidden')
-        rightPopupExpand.classList.add('hidden')
-    }
-    popupImage.src = images[numberOfMainImage]
-    mainImage.src = images[numberOfMainImage]
+popupImage.src = images[0];
+mainImage.addEventListener("click", () => {
+  popupWithImage.open(mainImage.src);
+});
+function nextImage() {
+  if (numberOfMainImage < images.length - 1) {
+    numberOfMainImage++;
+  }
+  if (numberOfMainImage !== 0) {
+    leftExpand.classList.remove("hidden");
+    leftPopupExpand.classList.remove("hidden");
+  }
+  if (numberOfMainImage === images.length - 1) {
+    rightExpand.classList.add("hidden");
+    rightPopupExpand.classList.add("hidden");
+  }
+  popupImage.src = images[numberOfMainImage];
+  mainImage.src = images[numberOfMainImage];
 }
-function prevImage(){
-    if(numberOfMainImage > 0){
-        numberOfMainImage--
-    }
-    if(numberOfMainImage === 0){
-        leftExpand.classList.add('hidden')
-        leftPopupExpand.classList.add('hidden')
-    }
-    rightExpand.classList.remove('hidden')
-    rightPopupExpand.classList.remove('hidden')
-    popupImage.src = images[numberOfMainImage]
-    mainImage.src = images[numberOfMainImage]
+function prevImage() {
+  if (numberOfMainImage > 0) {
+    numberOfMainImage--;
+  }
+  if (numberOfMainImage === 0) {
+    leftExpand.classList.add("hidden");
+    leftPopupExpand.classList.add("hidden");
+  }
+  rightExpand.classList.remove("hidden");
+  rightPopupExpand.classList.remove("hidden");
+  popupImage.src = images[numberOfMainImage];
+  mainImage.src = images[numberOfMainImage];
 }
-const rightExpand = document.querySelector('.main__image-expand_right');
-const leftExpand = document.querySelector('.main__image-expand_left');
-const leftPopupExpand = document.querySelector('.popup__expand_left');
-const rightPopupExpand = document.querySelector('.popup__expand_right')
-rightExpand.addEventListener('click', nextImage)
-leftExpand.addEventListener('click', prevImage)
-leftPopupExpand.addEventListener('click', prevImage)
-rightPopupExpand.addEventListener('click', nextImage)
-if(numberOfMainImage === 0){
-    leftExpand.classList.add('hidden')
-    leftPopupExpand.classList.add('hidden')
+const rightExpand = document.querySelector(".main__image-expand_right");
+const leftExpand = document.querySelector(".main__image-expand_left");
+const leftPopupExpand = document.querySelector(".popup__expand_left");
+const rightPopupExpand = document.querySelector(".popup__expand_right");
+rightExpand.addEventListener("click", nextImage);
+leftExpand.addEventListener("click", prevImage);
+leftPopupExpand.addEventListener("click", prevImage);
+rightPopupExpand.addEventListener("click", nextImage);
+if (numberOfMainImage === 0) {
+  leftExpand.classList.add("hidden");
+  leftPopupExpand.classList.add("hidden");
 }
-rightExpand.addEventListener('click', nextImage)
-leftExpand.addEventListener('click', prevImage)
-leftPopupExpand.addEventListener('click', prevImage)
-rightPopupExpand.addEventListener('click', nextImage)
-if(numberOfMainImage === 0){
-    leftExpand.classList.add('hidden')
-    leftPopupExpand.classList.add('hidden')
+rightExpand.addEventListener("click", nextImage);
+leftExpand.addEventListener("click", prevImage);
+leftPopupExpand.addEventListener("click", prevImage);
+rightPopupExpand.addEventListener("click", nextImage);
+if (numberOfMainImage === 0) {
+  leftExpand.classList.add("hidden");
+  leftPopupExpand.classList.add("hidden");
 }
 popupWithImage.setEventListeners();
-document.addEventListener('keydown', (evt) => {
-    if(evt.key === 'ArrowRight'){
-        nextImage();
-    } else if(evt.key === 'ArrowLeft'){
-        prevImage();
-    }
-})
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "ArrowRight") {
+    nextImage();
+  } else if (evt.key === "ArrowLeft") {
+    prevImage();
+  }
+});
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,50 @@
-import { Popup } from '../components/Popup.js';
-import { Product } from "./Product.js"
+import { Popup } from "../components/Popup.js";
+import { Product } from "./Product.js";
 import { Section } from "./Section.js";
-import { Footer } from '../components/Footer.js';
+import { Footer } from "../components/Footer.js";
 const footerInfo = new Footer();
 footerInfo.text();
 //попап просмотра изображения
-const popupWithImage = new Popup('.popup_main');
-const mainImage = document.querySelector('.about-us__image');
-const mainImageSrc = mainImage.src = './src/images/prospekt-2.jpeg'
-mainImage.addEventListener('click', () => {
-    popupWithImage.open(mainImageSrc)
-})
+const popupWithImage = new Popup(".popup_main");
+const mainImage = document.querySelector(".about-us__image");
+const mainImageSrc = (mainImage.src = "./src/images/prospekt-2.jpg");
+mainImage.addEventListener("click", () => {
+  popupWithImage.open(mainImageSrc);
+});
 popupWithImage.setEventListeners();
 
 // хиты продаж
 
 const products = [
-    {link: './perpetual-calendar.html',
+  {
+    link: "./perpetual-calendar.html",
     image: "./src/images/календарь 2.jpeg",
-    name: 'Вечные календари'
-    },
-    {link: './phone-stand.html',
+    name: "Вечные календари",
+  },
+  {
+    link: "./phone-stand.html",
     image: "./src/images/подставка 4.jpeg",
-    name: 'Подставки под телефон'
-    },
-    {link: './notebooks-catalog.html',
+    name: "Подставки под телефон",
+  },
+  {
+    link: "./notebooks-catalog.html",
     image: "./src/images/блокнот 1.jpeg",
-    name: 'Блокноты'
-    },
-    {link: './орешки-с-предсказанием.html',
+    name: "Блокноты",
+  },
+  {
+    link: "./орешки-с-предсказанием.html",
     image: "./src/images/орешек 1.jpeg",
-    name: 'Орешки с предсказанием'
-    },
-]
-const productsList = new Section({
+    name: "Орешки с предсказанием",
+  },
+];
+const productsList = new Section(
+  {
     items: products,
     renderer: (products) => {
-        const product = new Product(products, '.product');
-        productsList.addItem(product.generateProduct())
-    }
-}, '.bestsellers__items-container')
+      const product = new Product(products, ".product");
+      productsList.addItem(product.generateProduct());
+    },
+  },
+  ".bestsellers__items-container"
+);
 productsList.renderItems();
